4729829ecec1bb9687f16f072c643518403a1a18
Oliver <oli.mueller@gmail.com>
2022-07-11 11:56:51 +0200

    Create olis_utils.R
    Init

---
+++ olis_utils.R
@@ -0,0 +1,16 @@
+library(ggplot2)
+library(MetBrewer)
+
+### Customized theme
+theme_set(theme_void())
+theme_update(
+  axis.text.x = element_text(color = "black", face = "bold", size = 22, margin = margin(t = 6)),
+  axis.text.y = element_text(color = "black", size = 20, hjust = 1, margin = margin(r = 6)),
+  axis.line.x = element_line(color = "black", size = 1),
+  panel.grid.major.y = element_line(color = "grey90", size = .6),
+  plot.background = element_rect(fill = "white", color = "white"),
+  plot.margin = margin(rep(20, 4))
+)
+
+### Custom colors
+my_pal <- met.brewer(name="Austria", n=5, type="discrete")
